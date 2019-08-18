ba32df9393e1ef2da48131fbc667cdfa2799d8dd
Dan Nielsen <saattrupdan@gmail.com>
2019-08-18 21:38:19 +0100

    moved sandbox to NN.py

--- NN.py
+++ NN.py
@@ -373,3 +373,63 @@ class NeuralNetwork(TransformerMixin, BaseEstimator):
         X = np.asarray(X).reshape(self.layer_dims_[0], -1)
         Yhat, _ = forward_prop(X, self.params_, self.activations_)
         return Yhat
+
+
+if __name__ == '__main__':
+
+    # Application 1: regression on the squaring function
+
+    X = np.linspace(0, 1)
+    X = X.reshape(1, X.size)
+    Y = X ** 2
+    Y = Y.reshape(1, Y.size)
+
+    nn_model = NeuralNetwork(
+        layer_dims = [100, 100, 1], 
+        activations = ['relu', 'relu', 'sigmoid'],
+        cost_function = 'l2',
+        alpha = 0.1,
+        plot_cost = True,
+        num_iterations = 4000,
+        init_method = 'he'
+    )
+
+    nn_model.fit(X, Y)
+    Yhat = nn_model.predict(X)
+
+
+    # Application 2: separating points on a plane
+    
+    from itertools import product
+
+    X = np.array([[-4, -4], [-4, 4], [4, -4], [4, 4]]).T
+    y = np.array([[1], [0], [0], [1]]).T
+
+    nn_model = NeuralNetwork(
+        layer_dims = [5, 1],
+        activations = ['sigmoid', 'sigmoid'], 
+        cost_function = 'cross_entropy',
+        alpha = 0.1,
+        plot_cost = True,
+        num_iterations = 4000,
+        init_method = 'he'
+    )
+
+    nn_model.fit(X, y)
+
+    x = np.linspace(-10, 10, 200)
+    y = np.linspace(-10, 10, 200)
+    X, Y = np.meshgrid(x, y)
+    Z = np.zeros(X.shape)
+
+    for (i, j) in product(range(Z.shape[0]), range(Z.shape[1])):
+        Z[i,j] = nn_model.predict([[x[i]],[y[j]]])
+
+    plt.contourf(X, Y, Z, alpha=.5, cmap='jet_r')
+    plt.contour(X, Y, Z,  colors='black')
+    plt.plot([4,-4],[4,-4],'x',color='blue')
+    plt.plot([4,-4],[-4,4],'x',color='red')
+    plt.axis('equal')
+    plt.figtext(0.5, 0.01, 'Decision boundary of the fit for our NN.', 
+                wrap=True, horizontalalignment='center', fontsize=12)
+    plt.show()
